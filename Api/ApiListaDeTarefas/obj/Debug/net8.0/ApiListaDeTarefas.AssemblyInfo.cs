bd021d9d77c2a811f9b93081671806900073160f
GabrielSantos29 <142807683+GabrielSantos29@users.noreply.github.com>
2025-08-22 22:06:53 -0300

    salvando commit

--- Api/ApiListaDeTarefas/obj/Debug/net8.0/ApiListaDeTarefas.AssemblyInfo.cs
+++ Api/ApiListaDeTarefas/obj/Debug/net8.0/ApiListaDeTarefas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiListaDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38deea7628cef08fa504f707d684da83cba2e539")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb949c83a78e6ddd2d711af5848429973310fbf4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiListaDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiListaDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
